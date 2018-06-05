77a12a91a2c9cead6c0b4f376c42a089acb06bea
Eric Pandolfo <eric.pandolfo@gmail.com>
2018-06-05 11:31:49 -0300

    Versionando os exercicios atualizados

--- BancoDeDadosPostgres.sql
+++ BancoDeDadosPostgres.sql
@@ -1,7 +1,1 @@
-﻿--Exercicio 3
-begin;
-delete from uf where id in(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,26,27)
---ou
-delete from uf where id in (select uf.id from uf left join cidade c on c.uf_id = uf.id where c.uf_id is null)
-
-commit;
+﻿
